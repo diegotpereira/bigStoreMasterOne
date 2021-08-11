f20df8aafc1d57ec3a789f8586382e175fe885a1
Diego <diegoteixeirapereira@hotmail.com>
2021-08-11 17:17:05 -0300

    alteração nos arquivos de configuração.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,6 @@ const mix = require('laravel-mix');
 
 
  mix.js('resources/assets/js/app.js', 'public/js').vue()
- .js('resources/assets/js/bootstrap.js', 'public/js')
+ .js('resources/assets/js/bootstrap.js', 'public/js').sourceMaps()
  .sass('resources/assets/sass/app.scss', 'public/css');
 
