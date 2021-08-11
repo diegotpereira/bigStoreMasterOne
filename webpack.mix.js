bb783304ac43bfc2ffe6e7072b2327c8d47e0239
Diego <diegoteixeirapereira@hotmail.com>
2021-08-11 12:17:36 -0300

    alteração nos arquivos de configuração do projeto.

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,11 +6,13 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
- mix.js('resources/assets/js/app.js', 'public/js')
+
+ mix.js('resources/assets/js/app.js', 'public/js').vue()
  .js('resources/assets/js/bootstrap.js', 'public/js')
  .sass('resources/assets/sass/app.scss', 'public/css');
+
