b8ec2a4e4736d633f2422f4aba33cd0d135a61ef
Diego <diegoteixeirapereira@hotmail.com>
2021-08-11 12:14:51 -0300

    alteração no arquivo rotas movida para api.php.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('login', 'UserController@login');;
+Route::post('register', 'UserController@register');
+
+Route::get('/produtos', 'ProdutoController@index');
+Route::post('/upload-file', 'ProdutoController@uploadFile');
+Route::get('/produtos/{produto}', 'ProdutoController@show');
+
+route::group(['middleware' => 'auth:api'], function() {
+    Route::get('/users', 'UserController@index');
+    Route::get('/users/{user}', 'UserController@show');
+    Route::patch('users/{user}', 'UserController@update');
+    Route::get('users/{user}/pedidos', 'UserController@mostrarPedidos');
+
+    Route::patch('produtos/{produto}/unidades/add', 'ProdutoController@atualizarUnidades');
+
+    Route::patch('pedidos/{pedido}/entragar', 'PedidoController@entregarPedido');
+    Route::resource('/pedidos', 'PedidoController');
+    Route::resource('/produtos', 'ProdutoController')->except(['index', 'show']);
 });
--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any}', function () {
     return view('landing');
 })->where('any', '.*');
-
-
-Route::post('login', 'UserController@login');;
-Route::post('register', 'UserController@register');
-
-Route::get('/produtos', 'ProdutoController@index');
-Route::post('/upload-file', 'ProdutoController@uploadFile');
-Route::get('/produtos/{produto}', 'ProdutoController@show');
-
-route::group(['middleware' => 'auth:api'], function() {
-    Route::get('/users', 'UserController@index');
-    Route::get('/users/{user}', 'UserController@show');
-    Route::patch('users/{user}', 'UserController@update');
-    Route::get('users/{user}/pedidos', 'UserController@mostrarPedidos');
-
-    Route::patch('produtos/{produto}/unidades/add', 'ProdutoController@atualizarUnidades');
-
-    Route::patch('pedidos/{pedido}/entragar', 'PedidoController@entregarPedido');
-    Route::resource('/pedidos', 'PedidoController');
-    Route::resource('/produtos', 'ProdutoController')->except(['index', 'show']);
-});
